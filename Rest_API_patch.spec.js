e77bc1d0fbf48d1869579e88b7cefcf3050f9ab4
Chahat <chahat922001@gmail.com>
2026-01-21 12:37:29 +0530

    add patch file

--- Rest_API_patch.spec.js
+++ Rest_API_patch.spec.js
@@ -1,1 +1,21 @@
 import {test, expect} from '@playwright/test'
+test('patch method', async({request})=>{
+    const response= await request.patch("https://jsonplaceholder.typicode.com/posts/1",{
+    headers:{
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+    data: {
+    title: 'foo',
+    },
+})
+
+    expect (response.status()).toBe(200)
+
+    const responseData= await response.json()
+
+    expect(responseData.title).toBe('foo')
+
+    const responseHeaders = await response.headers()
+    expect(responseHeaders['content-type']).toContain('application/json')
+
+})
