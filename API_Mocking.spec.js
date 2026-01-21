ac9fbf395e900f397e92354ef8b5cf6d44824269
Chahat <chahat922001@gmail.com>
2026-01-21 19:07:44 +0530

    update mocking file

--- API_Mocking.spec.js
+++ API_Mocking.spec.js
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 test('mock a fruit list and does not call the actual API', async({page})=>{
-    await page.route('*/**/api/v1/fruits', async route=>{
-        const json= [{name:'Testing', id:30}]
+    await page.route('**/api/v1/fruits', async route=>{
+        const json= [{'name':'Testing', id:30}]
         await route.fulfill({json})
     }
     )
